19d3e0fc2725ad7700f01bda298667ac061969f7
Matías Lafroce <matias@mini-parche>
2017-10-30 00:27:29 -0300

    Refactor de cRadix
    Modulariza la llamada a radixSort en C

--- dc3/cRadix.py
+++ dc3/cRadix.py
@@ -18,38 +18,28 @@ void radixSortB0(MyVector indexes, MyVector values, int radixSize)
 '''
 _radixDLL.radixSortB0.argtypes = (CVector, CVector, ctypes.c_int)
 
+def listToCVector(pyList):
+    # Declaro el tipo array de enteros
+    typeArray = ctypes.c_int * len(pyList)
+    cArray = typeArray(*pyList)
+    # Version CType del size del array
+    cSize = ctypes.c_int(len(pyList))
+    return CVector(cArray, cSize)
 
 def dc3RadixB12(indexes, values, radixSize):
-    # Declaro el tipo array de enteros
-    indexesArray = ctypes.c_int * len(indexes)
-    valuesArray = ctypes.c_int * len(values)
-    cIndexes = indexesArray(*indexes)
-    cValues = valuesArray(*values)
-
-    # Version CType del size del array
-    cIndexesSize = ctypes.c_int(len(indexes))
-    cValuesSize = ctypes.c_int(len(values))
-
-    cIndexesVector = CVector(cIndexes, cIndexesSize)
-    cValuesVector = CVector(cValues, cValuesSize)
+    
+    cIndexesVector = listToCVector(indexes)
+    cValuesVector = listToCVector(values)
     cRadixSize = ctypes.c_int(radixSize)
+    #_radixDLL.ProfilerStart('out.prof')
     result = _radixDLL.radixSort(
         cIndexesVector, cValuesVector, cRadixSize)
+    #_radixDLL.ProfilerStop('out.prof')
     _convertCtoPython(indexes, cIndexesVector.data)
 
 def dc3RadixB0(indexes, values, radixSize):
-    # Declaro el tipo array de enteros
-    indexesArray = ctypes.c_int * len(indexes)
-    valuesArray = ctypes.c_int * len(values)
-    cIndexes = indexesArray(*indexes)
-    cValues = valuesArray(*values)
-
-    # Version CType del size del array
-    cIndexesSize = ctypes.c_int(len(indexes))
-    cValuesSize = ctypes.c_int(len(values))
-
-    cIndexesVector = CVector(cIndexes, cIndexesSize)
-    cValuesVector = CVector(cValues, cValuesSize)
+    cIndexesVector = listToCVector(indexes)
+    cValuesVector = listToCVector(values)
     cRadixSize = ctypes.c_int(radixSize)
     result = _radixDLL.radixSortB0(
         cIndexesVector, cValuesVector, cRadixSize)
